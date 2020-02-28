268451ffdded9bd10195d3b12643158701ad56b4
root <marion.pages01@etu.umontpellier.fr>
2020-02-28 16:54:35 +0100

    Avancée et modifications

--- script_meteo.sh
+++ script_meteo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-#echo $(weather lfmu)
-#echo $(weather lfmu | awk '{print $3}' | head -n 4 | tail -n1 )
-echo "Voici la météo du jour : " > meteo.txt
+#echo "Nous sommes le : $(weather lfmu | awk '{print $4}' | cut -c1-2 | head -n 2 | tail -n 1) $(weather lfmu | awk '{print $3}' | head -n 2 | tail -n 1 | sed s/Feb/Février/g) $(weather lfmu | awk '{print $5}' | head -n 2 | tail -n 1)" > meteo.txt
+#echo $(date)
+echo "Nous sommes le : $(date | cut -c1-25) il est $(date | cut -c28-32 | sed "s/:/ heure /g")" > meteo.txt
+echo "Voici la météo du jour : " >> meteo.txt
 echo "Lhumidité est de : $(weather lfmu | awk '{print $3}' | head -n 4 | tail -n 1)" >> meteo.txt
-echo "La température est de : $(weather lfmu | awk '{print $4,$5}' | head -n 3 | tail -n 1 | cut -c2-4)" >> meteo.txt
-echo "Le vent est de : $(weather lfmu | awk '{print $8,$9}' | head -n 5 | tail -n 1)" >> meteo.txt
+echo "La température est de : $(weather lfmu | awk '{print $4,$5}' | head -n 3 | tail -n 1 | cut -c2-3) degrés" >> meteo.txt
+vent_mph=$(weather lfmu | awk '{print $8}' | head -n 5 | tail -n 1)
+vent_kmh=$(echo "$vent_mph*1.60934" | bc | awk '{printf("%d\n",$1 + 0.5)}')
+echo "Le vent est de : $vent_kmh kilomètres par heure" >> meteo.txt 
